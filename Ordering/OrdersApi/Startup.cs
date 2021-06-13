f7105ac95f6cf01c74182b10a332f8f9d646b99b
reenamittal87 <reenamittal87@gmail.com>
2021-06-13 13:39:36 +0530

    MassTransit RabbitMQ and MVC web app

---
+++ Ordering/OrdersApi/Startup.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using Messaging.InterfacesConstants.Constants;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+/* using OrdersApi.Messages.Consumers;
+using OrdersApi.Persistence;
+using OrdersApi.Services; */
+
+namespace OrdersApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<OrdersContext>(options => options.UseSqlServer
+            (
+                Configuration.GetConnectionString("OrdersContextConnection")
+            ));
+
+            services.AddHttpClient();
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
+            services.AddMassTransit(
+                c =>
+                {
+                    c.AddConsumer<RegisterOrderCommandConsumer>();
+                } );
+               
+
+            services.AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+             {
+                 var host = cfg.Host("localhost", "/", h => { });
+                 cfg.ReceiveEndpoint(RabbitMqMassTransitConstants.RegisterOrderCommandQueue, e =>
+                 {
+                     e.PrefetchCount = 16;
+                     e.UseMessageRetry(x => x.Interval(2, TimeSpan.FromSeconds(10)));
+                     e.Consumer<RegisterOrderCommandConsumer>(provider);
+
+                 });
+
+
+                 cfg.ConfigureEndpoints(provider);
+             }));
+            services.AddSingleton<IHostedService, BusService>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OrdersApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrdersApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
